6610f67d8e3ba9b97be0aa4d2001ed0930cb2410
JackEvans0317 <satriforce@gmail.com>
2020-02-06 12:41:01 -0600

    FINISHED ASSIGNMENT

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -32,10 +32,11 @@ public class HomeController {
     private JobRepository jobRepository;
 
 
-    @RequestMapping("")
+    @GetMapping
     public String index(Model model) {
 
         model.addAttribute("title", "My Jobs");
+        model.addAttribute("jobs",jobRepository.findAll());
 
         return "index";
     }
@@ -68,9 +69,13 @@ public class HomeController {
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
-
-        return "view";
+        Optional optJob = jobRepository.findById(jobId);
+        if(optJob.isPresent()) {
+            Job job = (Job) optJob.get();
+            model.addAttribute("job", job);
+            return "view";
+        } else {
+            return "redirect:";
+        }
     }
-
-
 }
